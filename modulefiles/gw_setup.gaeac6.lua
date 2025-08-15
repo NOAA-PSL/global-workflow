3b7844f9799895b32e38c6287f2dcc24a29ed1c6
David Huber <69919478+DavidHuber-NOAA@users.noreply.github.com>
2025-08-15 17:49:41 +0000

    spack-stack 1.9.2

--- modulefiles/gw_setup.gaeac6.lua
+++ modulefiles/gw_setup.gaeac6.lua
@@ -5,7 +5,7 @@ Load environment to run GFS workflow setup scripts on Gaea C6
 prepend_path("MODULEPATH", "/autofs/ncrc-svm1_proj/hurr1/hafs/shared/modulefiles")
 load(pathJoin("rocoto", "1.3.7_fix"))
 
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/c6/spack-stack-1.9.2/envs/ue-intel-2023.2.0/install/modulefiles/Core")
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
 local python_ver=os.getenv("python_ver") or "3.11.7"
